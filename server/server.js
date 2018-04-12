3baf915fe7a90a1c0a3a85b3c3bdfd250fbfb90b
Amanda <albullington@gmail.com>
2018-04-12 13:54:35 -0700

    made changes based on code review

--- server/server.js
+++ server/server.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 const port = 3002;
 const app = express();
 
+app.use(express.static(__dirname + '../client/dist'));
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('GET request received');
