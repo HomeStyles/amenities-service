0307fce8af5fdee2b74f853764e53a237d4ed2bd
Amanda <albullington@gmail.com>
2018-04-13 12:20:05 -0700

    using this.state instead of strictly hardcoding

--- client/src/components/index.jsx
+++ client/src/components/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styled from 'styled-components';
+import axios from 'axios';
 
 const Box = styled.div`
   float: left;
@@ -45,6 +46,40 @@ const Text = styled.div`
 `;
 
 class Description extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      homeName: 'The best house',
+      description: 'Really, the greatest house',
+      location: 'Norway',
+      viewsThisWeek: 200,
+      petsAllowed: false,
+      checkOut: '11AM',
+      checkIn: '2PM',
+      amenities: {
+        wifi: true,
+        hotWater: false,
+        shampoo: false,
+        towels: true,
+        sheets: true,
+        kitchen: false,
+      },
+      rooms: {
+        totalBedrooms: 4,
+        totalBeds: 4,
+        totalBaths: 2,
+      },
+      host: {
+        hostId: 123,
+        firstName: 'Jack',
+        lastName: 'Sparrow',
+        image: '',
+        email: 'jacksparrow@123.com',
+      },
+    };
+  }
+
   render() {
     return (
       <div>
@@ -52,30 +87,31 @@ class Description extends React.Component {
           <div className="houseType">ENTIRE APARTMENT</div>
         </BoldText>
         <Name>
-          <div className="homeName"><h2>The best house</h2></div>
+          <div className="homeName"><h2>{this.state.homeName}</h2></div>
         </Name>
         <Text>
-          <div className="location">Norway</div>
-          <div className="stats">4 guests, Studio, 2 beds</div>
+          <div className="location">{this.state.location}</div>
+          <div className="stats">{this.state.rooms.totalBedrooms} bedrooms, {this.state.rooms.totalBeds} beds, {this.state.rooms.totalBaths} baths</div>
         </Text>
         <Box>
           <Text>
-            <div className="views">200 this week</div>
+            <div className="views">This home is top of mind. Over {this.state.viewsThisWeek} people have viewed it in the past week.</div>
           </Text>
         </Box>
         <Text>
-          <div className="description">Really, the greatest house</div>
+          <div className="description">{this.state.description}</div>
           <div className="amenities"><h4>Amenities</h4>Kitchen, wifi, iron</div>
           <div className="sleepingArrangements"><h4>Sleeping arrangements</h4>
             <Box>
             1 double bed, 1 sofa bed
             </Box>
           </div>
-          <div className="houseRules"><h4>House Rules</h4>No parties.</div>
+          <div className="houseRules"><h4>House Rules</h4>Pets are not allowed. Check in time is {this.state.checkIn}. Check out by {this.state.checkOut}.</div>
         </Text>
       </div>
     );
   }
 }
+
   
 ReactDOM.render(<Description />, document.getElementById('description'));
--- tests/test.js
+++ tests/test.js
@@ -5,23 +5,23 @@ test('the array has 100 items', () => {
 });
 
 test('total bedrooms are at least 1', () => {
-	for (let i = 0; i < arr.length; i++) {
-		expect(arr[i].rooms.totalBedrooms).toBeGreaterThanOrEqual(1);
-	}
+  for (let i = 0; i < arr.length; i++) {
+    expect(arr[i].rooms.totalBedrooms).toBeGreaterThanOrEqual(1);
+  }
 });
 
 test('each home has the following properties', () => {
-	for (let i = 0; i < arr.length; i++) {
-		expect(arr[i]).toHaveProperty('homeName');
-		expect(arr[i]).toHaveProperty('checkOut');
-		expect(arr[i]).toHaveProperty('amenities.hotWater');
-		expect(arr[i]).toHaveProperty('host.image');
-	}
+  for (let i = 0; i < arr.length; i++) {
+    expect(arr[i]).toHaveProperty('homeName');
+    expect(arr[i]).toHaveProperty('checkOut');
+    expect(arr[i]).toHaveProperty('amenities.hotWater');
+    expect(arr[i]).toHaveProperty('host.image');
+  }
 });
 
 test('data set is filled with unique objects', () => {
-	for (let i = 0; i < arr.length; i++) {
-		expect(arr[3]).not.toMatchObject(arr[7]);
-		expect(arr[43]).not.toMatchObject(arr[78]);
-	}
+  for (let i = 0; i < arr.length; i++) {
+    expect(arr[3]).not.toMatchObject(arr[7]);
+    expect(arr[43]).not.toMatchObject(arr[78]);
+  }
 });
