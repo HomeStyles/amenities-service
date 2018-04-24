ca686d4e08104925b4812fbdaca1e37b4c4cf3c8
Amanda <albullington@gmail.com>
2018-04-24 11:15:50 -0700

    added cors

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const {Home} = require('../database/index');
 const app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
